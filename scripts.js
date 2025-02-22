5126530c0a92f17c89d8a32e3f5a500db8d4bc3a
TimurOzer <hakonet72@hotmail.com>
2025-02-22 11:33:52 +0300

    push

---
+++ scripts.js
@@ -0,0 +1,4 @@
+// Basit bir kontrol: Sayfa yüklendiğinde konsola mesaj yazdırıyoruz.
+document.addEventListener("DOMContentLoaded", function() {
+  console.log("Script yüklendi. Web siteniz hazır!");
+});
